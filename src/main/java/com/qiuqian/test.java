0985bf4cea4d3851cdc7005bb2f40f6e1c7c4a6c
qiuqian <qiu1641118989@163.com>
2022-10-16 11:47:35 +0800

    test1

--- src/main/java/com/qiuqian/test.java
+++ src/main/java/com/qiuqian/test.java
@@ -3,5 +3,9 @@ package com.qiuqian;
 public class test {
     public static void main(String[] args) {
         System.out.println("这是一个java初学者的练习");
+
+    }
+    public void test1(){
+        System.out.println("test1");
     }
 }
